10de88e90d635bc8601479cb7c46a99d3d20f169
Debi <debidutt.prasad@nl.abnamro.com>
2019-04-17 01:04:13 +0200

    meme text top and bottom implemented

---
+++ Meme/MemeTextDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  MemeTextDelegate.swift
+//  Meme
+//
+//  Created by Debidutt Prasad on 15/04/2019.
+//  Copyright © 2019 Bot Consultancy. All rights reserved.
+//
+
+import Foundation
--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:3.0
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        topText.delegate = self
+        bottomText.delegate = self
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,11 +51,20 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         if let image = info [.originalImage] as? UIImage {
         imagePickerView.image = image
         }
+        dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
